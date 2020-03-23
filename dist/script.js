c26dff3af73d71c1b5a44a070485ae85f355ec83
mahmudz <mahmudbappy.pri@gmail.com>
2020-03-23 12:33:15 +0600

    Updated dist files

--- dist/script.js
+++ dist/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	shp("data.zip").then(function(geojson) {
 		let points = [
 			[-81.03459538464868, 29.2676691091238],
+			[-81.070634, 29.321528],
 			[-81.127800, 29.305349],
 			[-81.129795, 29.337322],
 			[-81.115580, 29.316688],
@@ -10,10 +11,10 @@ $(document).ready(function() {
 			[-81.125568, 29.270802],
 			[-81.0336497, 29.2709837],
 			[-81.0305707, 29.2699357],
-			[-81.0282637, 29.2643857]
+			[-81.0282637, 29.2643857],
+			[-81.124464, 29.333577]
 		];
 
-		// console.log(inGeojsonArea(points[0], geojson[0]));
 		var map = new mapboxgl.Map({
 			container: 'map',
 			style: 'mapbox://styles/mapbox/outdoors-v11',
@@ -34,48 +35,66 @@ $(document).ready(function() {
 			});
 			map.addLayer({
 				'id': id,
+				'type': 'fill',
+				'source': id,
+				'paint': {
+					'fill-color': '#1A73E8',
+					'fill-opacity': 0.2
+				},
+				'filter': ['==', '$type', 'Polygon']
+			});
+
+			map.addLayer({
+				'id': id + '_line',
 				'type': 'line',
 				'source': id,
 				'paint': {
-					'line-color': '#75CFF0'
+					'line-color': '#1A73E8'
 				},
 				'filter': ['==', '$type', 'Polygon']
+			});
+
+			map.addSource('sourceTwo', {
+				'type': 'geojson',
+				'data': geojson[1]
 			});
 			map.addLayer({
-				'id': id,
-				'type': 'line',
-				'source': id,
+				'id': 'layerTwo',
+				'type': 'fill',
+				'source': 'sourceTwo',
 				'paint': {
-					'line-color': '#75CFF0'
+					'fill-color': '#26528C',
+					'fill-opacity': 0.2
 				},
 				'filter': ['==', '$type', 'Polygon']
 			});
 
-			let coordinatesSets = geojson[0].features[0].geometry.coordinates;
-			var polygons = [];
-			coordinatesSets.forEach(set => {
-				polygons.push(turf.polygon(set));
-			})
-			var inside = false;
-			for (var i = 0; i < points.length; i++) {
-				for (var j = 0; j < polygons.length; j++) {
-					if (turf.booleanPointInPolygon(turf.point(points[i]), polygons[j])) {
-						inside = true;
-						break;
-					}
-				}
+			map.addLayer({
+				'id': 'layerTwo_line',
+				'type': 'line',
+				'source': 'sourceTwo',
+				'paint': {
+					'line-color': '#4289E4'
+				},
+				'filter': ['==', '$type', 'Polygon']
+			});
 
-				if(inside) {
-					marker.push(new mapboxgl.Marker().setLngLat(points[i]).addTo(map));
+
+			var pointsPos = inGeojsonArea.areThey(points, geojson[0]);
+			console.log(pointsPos);
+
+			var isThis = inGeojsonArea.isThis(points[2], geojson[0]);
+			console.log(isThis);
+			
+			pointsPos.forEach(point => {
+				if(point.isInside) {
+					marker.push(new mapboxgl.Marker().setLngLat(point.point).addTo(map));
 				}else{
 					marker.push(new mapboxgl.Marker({
 						color: '#FFB1CE'
-					}).setLngLat(points[i]).addTo(map));
+					}).setLngLat(point.point).addTo(map));
 				}
-				inside = false;
-			}
-
-
+			});
 		});
 	});
 });
