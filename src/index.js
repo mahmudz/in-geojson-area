1c80f17dc51c3e21490844afe0a1a5f5d7488940
mahmudz <mahmudbappy.pri@gmail.com>
2020-03-23 12:27:55 +0600

    Full support for polygons and multipolygons

--- src/index.js
+++ src/index.js
@@ -1,42 +1,63 @@
 function toPolygons(geojson) {
-	var polygons = [];
-	let coordinatesSets = geojson.features[0].geometry.coordinates;
-	coordinatesSets.forEach(set => {
-		polygons.push(turf.polygon(set));
-	})
-
-	return polygons;
+	try {
+		var polygons = [];
+		geojson.features.forEach(feature => {
+			if(feature.type == 'Feature') {
+				if(feature.geometry.type == 'Polygon') {
+					polygons.push(turf.polygon([feature.geometry.coordinates[0]]));
+				}else if(feature.geometry.type == 'MultiPolygon'){
+					let coordinatesSets = feature.geometry.coordinates;
+					coordinatesSets.forEach(set => {
+						polygons.push(turf.polygon(set));
+					})
+				}
+			}
+		})
+		
+		return polygons;
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 function areThey(points, geojson) {
-	let polygons = toPolygons(geojson);
-	let result = [];
-	let isInside = false;
-	points.forEach(point => {
+	try {
+		let polygons = toPolygons(geojson);
+		let result = [];
+		let isInside = false;
+		points.forEach(point => {
+			for (var i = 0; i < polygons.length; i++) {
+				if (turf.booleanPointInPolygon(turf.point(point), polygons[i])) {
+					isInside = true;
+					break;
+				}
+			}
+			result.push({ point, isInside })
+			isInside = false;
+		})
+
+		return result;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function isThis(point, geojson) {
+	try {
+		let polygons = toPolygons(geojson);
+		let isInside = false;
+
 		for (var i = 0; i < polygons.length; i++) {
 			if (turf.booleanPointInPolygon(turf.point(point), polygons[i])) {
 				isInside = true;
 				break;
 			}
 		}
-		result.push({ point, isInside })
-	})
 
-	return result;
-}
-
-function isThis(point, geojson) {
-	let polygons = toPolygons(geojson);
-	let isInside = false;
-
-	for (var i = 0; i < polygons.length; i++) {
-		if (turf.booleanPointInPolygon(turf.point(point), polygons[i])) {
-			isInside = true;
-			break;
-		}
+		return isInside;
+	} catch (error) {
+		console.log(error);
 	}
-
-	return isInside;
 }
 
 
